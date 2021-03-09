e13705b64f6913174293fcc2c00eb8a2e1912eff
yvonneeeee <yvonnelin840701@gmail.com>
2021-03-09 05:41:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,8 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
+function addElementToBeginningOfArray(array,element){
+  return [element, ...array]
+}
 
 function destructivelyAddElementToBeginningOfArray(array,element){
   array.unshift(elememt);
